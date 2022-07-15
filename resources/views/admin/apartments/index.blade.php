f033449cdbd831fd7945f3d3eda2a491da1eb44a
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-07-15 17:31:29 +0200

    create-index-page

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,7 +1,89 @@
 @extends('layouts.admin')
 @section('content')
-<h1>index</h1>
-@foreach($apartments as $apartment)
-<a href="{{route('admin.apartments.edit', $apartment->id)}}">Edit {{$apartment->name}}</a>
-@endforeach
-@endsection
+<div class="container">
+    <h1 class="text-center">All Apartments</h1>
+    <table class="table table-striped table-inverse table-responsive">
+        <thead class="thead-inverse">
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Rooms</th>
+                <th>Beds</th>
+                <th>Bathrooms</th>
+                <th>Square_meters</th>
+                <th>Address</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+                <th>Cover image</th>
+                <th>Description</th>
+                <th>Visibile</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($apartments as $apartment)
+            <tr>
+                <td scope="row">{{$apartment->id}}</td>
+                <td>{{$apartment->title}}</td>
+                <td>{{$apartment->rooms}}</td>
+                <td>{{$apartment->beds}}</td>
+                <td>{{$apartment->bathrooms}}</td>
+                <td>{{$apartment->square_meters}} m²</td>
+                <td>{{$apartment->address}}</td>
+                <td>{{$apartment->latitude}}</td>
+                <td>{{$apartment->longitude}}</td>
+                <td>
+                    <img width="120" height="80" src="{{asset('storage/' . $apartment->cover_image)}}" alt="">
+                </td>
+                <td>{{substr($apartment->description,0,20)}}...</td>
+                <td>{{$apartment->visible}}</td>
+                <td>
+                    <a class="btn btn-primary" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                        </svg>
+                    </a>
+                    <a class="btn btn-secondary" href="{{route('admin.apartments.edit',$apartment->id)}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                        </svg>
+                    </a>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modelApartment{{$apartment->id}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                        </svg>
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="modelApartment{{$apartment->id}}" tabindex="-1" role="dialog" aria-labelledby="modelApartmentId" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Delete {{$apartment->title}}</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure to Delete {{$apartment->title}}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                    <button type="button" class="btn btn-danger">Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td scope="row">No apartment yet</td>
+            </tr>
+            @endforelse
+            @endsection
+        </tbody>
+    </table>
+</div>
