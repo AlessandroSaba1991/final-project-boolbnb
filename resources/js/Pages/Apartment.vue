50111f605d97a30cb39d702b3f8b5513ee5c2dc7
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-07-18 10:48:40 +0200

    create view apartment

--- resources/js/Pages/Apartment.vue
+++ resources/js/Pages/Apartment.vue
@@ -1,16 +1,44 @@
 <template>
-<div class="single_page h-100">
+  <div class="single_page h-100">
     <div class="wrapper h-100" v-if="!loading">
       <div
         class="p-5 hero_image"
         :style="{
           backgroundImage: 'url(/storage/' + apartment.cover_image + ')',
         }"
-      >
-        <div class="container">
+      ></div>
+      <div class="container">
+        <div class="card p-3">
+            <div class="row row-cols-4">
+                <div class="col">
+                    <span>Rooms: </span>{{apartment.rooms}}
+                </div>
+                <div class="col">
+                    <span>Beds: </span>{{apartment.beds}}
+                </div>
+                <div class="col">
+                    <span>Bathrooms: </span>{{apartment.bathrooms}}
+                </div>
+                <div class="col">
+                    <span>Square_meters: </span>{{apartment.square_meters}}
+                </div>
+                <div class="col">
+                    <span>Address: </span>{{apartment.address}}
+                </div>
+                <div class="col">
+                    <span>Latitude: </span>{{apartment.latitude}}
+                </div>
+                <div class="col">
+                    <span>Longitude: </span>{{apartment.longitude}}
+                </div>
+                <div class="col">
+                    <span>Visible: </span>
+                    <span v-if="apartment.visible">Visibile</span>
+                    <span v-else>Non Visibile</span>
+                </div>
+            </div>
           <h1 class="display-3">{{ apartment.title }}</h1>
           <p class="lead">{{ apartment.description }}</p>
-          <hr class="my-2 bg-white" />
         </div>
       </div>
     </div>
@@ -20,8 +48,8 @@
 
 <script>
 export default {
-    name:'Apartment',
-    data() {
+  name: "Apartment",
+  data() {
     return {
       apartment: "",
       loading: true,
@@ -32,7 +60,7 @@ export default {
       axios
         .get("/api/apartments/" + this.$route.params.id)
         .then((response) => {
-            console.log(response.data);
+          console.log(response.data);
           if (response.data.status_code === 404) {
             this.loading = false;
             this.$router.push({ name: "not-found" });
@@ -49,9 +77,14 @@ export default {
   mounted() {
     this.getApartment();
   },
-}
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.hero_image {
+  height: 500px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 </style>
