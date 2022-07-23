ac03fa887aa08cb499c8fb878450ad272b6f0b5f
Rostrv <trovatorosario91@gmail.com>
2022-07-23 10:51:52 +0200

    modified index, edit, create

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,100 +1,125 @@
 @extends('layouts.admin')
-@section('content')
-<div class="container">
-    <h1 class="text-center">All Apartments</h1>
-    @include('partials.message')
-    <table class="table table-striped table-inverse table-responsive">
-        <thead class="thead-inverse">
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Rooms</th>
-                <th>Beds</th>
-                <th>Bathrooms</th>
-                <th>Square_meters</th>
-                <th>Address</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Cover image</th>
-                <th>Description</th>
-                <th>Visibile</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($apartments as $apartment)
-            <tr>
-                <td scope="row">{{$apartment->id}}</td>
-                <td>{{$apartment->title}}</td>
-                <td>{{$apartment->rooms}}</td>
-                <td>{{$apartment->beds}}</td>
-                <td>{{$apartment->bathrooms}}</td>
-                <td>{{$apartment->square_meters}} m²</td>
-                <td>{{$apartment->address}}</td>
-                <td>{{$apartment->latitude}}</td>
-                <td>{{$apartment->longitude}}</td>
-                <td>
-                    <img width="120" height="80" src="{{asset('storage/' . $apartment->cover_image)}}" alt="">
-                </td>
-                <td>{{substr($apartment->description,0,20)}}...</td>
-                @if($apartment->visible)
-                <td>Si</td>
-                @else
-                <td>No</td>
-                @endif
-                <td>
-                    <a class="btn btn-success" href="{{route('admin.messages.index', $apartment->id)}}">Message</a>
-                    <a class="btn btn-warning" href="{{route('admin.sponsorizations.index', $apartment->id)}}">Sponsorization</a>
-                    <a class="btn btn-primary" href="{{route('admin.apartments.show', $apartment->id)}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                        </svg>
-                    </a>
-                    <a class="btn btn-secondary my-1" href="{{route('admin.apartments.edit',$apartment->id)}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                        </svg>
-                    </a>
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modelApartment{{$apartment->id}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                        </svg>
-                    </button>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="modelApartment{{$apartment->id}}" tabindex="-1" role="dialog" aria-labelledby="modelApartmentId" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">Delete {{$apartment->title}}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Are you sure to Delete {{$apartment->title}}
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                    <form action="{{route('admin.apartments.destroy', $apartment->id)}}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                    </form>
+<link rel="stylesheet" href="{{ asset('css/admin_cards.css') }}">
+
+@section('content')
+    <div class="container">
+
+        @include('partials.message')
+        <div class="row">
+            @if (count($apartments) > 0)
+                <h1 class="text-center text-uppercase">I tuoi annunci</h1>
+                <div class="d-flex justify-content-center my-5">
+                    <a href="{{ route('admin.apartments.create') }}" class="btn btn-warning text-white text-uppercase">Crea
+                        un annuncio</a>
+                </div>
+            @endif
+            @forelse ($apartments as $apartment)
+                <div class=" col-xs-2 col-sm-6 col-md-6 col-lg-4 col-xl-3 g-4">
+                    <div class="card rounded-3 shadow bg-body" style="height:100%">
+                        <img src="{{ asset('storage/' . $apartment->cover_image) }}" class="card-img-top p-1"
+                            alt="...">
+                        @if ($apartment->visible)
+                            <span class="position-absolute top-0 badge bg-success">Visibile</span>
+                        @else
+                            <span class="position-absolute top-0 badge bg-danger">Non Visibile</span>
+                        @endif
+                        <div class="card-body">
+                            <h3 class="card-title text-uppercase">{{ $apartment->title }}</h3>
+
+                            <div class="mini_title">Numero camere:<span class="mini_subtitle">{{ $apartment->rooms }}</span>
+                            </div>
+                            <div class="mini_title">Posti letto:<span class="mini_subtitle">{{ $apartment->beds }}</span>
+                            </div>
+                            <div class="mini_title">Numero Bagni:<span
+                                    class="mini_subtitle">{{ $apartment->bathrooms }}</span></div>
+                            <div class="mini_title">Metri quadri:<span class="mini_subtitle">{{ $apartment->square_meters }}
+                                    m²</span></div>
+                            <div class="mini_title">Indirizzo:<span class="mini_subtitle">{{ $apartment->address }}</span>
+                            </div>
+
+                            <div class="d-flex justify-content-between flex-wrap align-items-center mt-3">
+                                <a class="btn btn-success" href="{{ route('admin.messages.index', $apartment->id) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16"
+                                        height="16" fill="white">
+                                        <path
+                                            d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z" />
+                                    </svg></a>
+                                <a class="btn btn-warning"
+                                    href="{{ route('admin.sponsorizations.index', $apartment->id) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="16"
+                                        height="16" fill="white">
+                                        <path
+                                            d="M512 64C547.3 64 576 92.65 576 128V384C576 419.3 547.3 448 512 448H64C28.65 448 0 419.3 0 384V128C0 92.65 28.65 64 64 64H512zM272 192C263.2 192 256 199.2 256 208C256 216.8 263.2 224 272 224H496C504.8 224 512 216.8 512 208C512 199.2 504.8 192 496 192H272zM272 320H496C504.8 320 512 312.8 512 304C512 295.2 504.8 288 496 288H272C263.2 288 256 295.2 256 304C256 312.8 263.2 320 272 320zM164.1 160C164.1 148.9 155.1 139.9 143.1 139.9C132.9 139.9 123.9 148.9 123.9 160V166C118.3 167.2 112.1 168.9 108 171.1C93.06 177.9 80.07 190.5 76.91 208.8C75.14 219 76.08 228.9 80.32 237.8C84.47 246.6 91 252.8 97.63 257.3C109.2 265.2 124.5 269.8 136.2 273.3L138.4 273.9C152.4 278.2 161.8 281.3 167.7 285.6C170.2 287.4 171.1 288.8 171.4 289.7C171.8 290.5 172.4 292.3 171.7 296.3C171.1 299.8 169.2 302.8 163.7 305.1C157.6 307.7 147.7 309 134.9 307C128.9 306 118.2 302.4 108.7 299.2C106.5 298.4 104.3 297.7 102.3 297C91.84 293.5 80.51 299.2 77.02 309.7C73.53 320.2 79.2 331.5 89.68 334.1C90.89 335.4 92.39 335.9 94.11 336.5C101.1 339.2 114.4 343.4 123.9 345.6V352C123.9 363.1 132.9 372.1 143.1 372.1C155.1 372.1 164.1 363.1 164.1 352V346.5C169.4 345.5 174.6 343.1 179.4 341.9C195.2 335.2 207.8 322.2 211.1 303.2C212.9 292.8 212.1 282.8 208.1 273.7C204.2 264.7 197.9 258.1 191.2 253.3C179.1 244.4 162.9 239.6 150.8 235.9L149.1 235.7C135.8 231.4 126.2 228.4 120.1 224.2C117.5 222.4 116.7 221.2 116.5 220.7C116.3 220.3 115.7 219.1 116.3 215.7C116.7 213.7 118.2 210.4 124.5 207.6C130.1 204.7 140.9 203.1 153.1 204.1C157.5 205.7 171 208.3 174.9 209.3C185.5 212.2 196.5 205.8 199.3 195.1C202.2 184.5 195.8 173.5 185.1 170.7C180.7 169.5 170.7 167.5 164.1 166.3L164.1 160z" />
+                                    </svg></a>
+                                <a class="btn btn-primary" href="{{ route('admin.apartments.show', $apartment->id) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                        <path
+                                            d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                        <path
+                                            d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                    </svg>
+                                </a>
+                                <a class="btn btn-secondary my-1"
+                                    href="{{ route('admin.apartments.edit', $apartment->id) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                        <path fill-rule="evenodd"
+                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                    </svg>
+                                </a>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modelApartment{{ $apartment->id }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                    </svg>
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="modelApartment{{ $apartment->id }}" tabindex="-1"
+                                    role="dialog" aria-labelledby="modelApartmentId" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Delete {{ $apartment->title }}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Are you sure to Delete {{ $apartment->title }}
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Cancel</button>
+                                                <form action="{{ route('admin.apartments.destroy', $apartment->id) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                </td>
-            </tr>
+                </div>
             @empty
-            <tr>
-                <td scope="row">No apartment yet</td>
-            </tr>
+                <div>
+                    <h2 class="text-center mb-3">Non hai ancora inserito un annuncio, fallo subito!</h2>
+                    <div class="d-flex justify-content-center">
+                        <a class="button_glow text-uppercase" href="{{ route('admin.apartments.create') }}">Clicca
+                            qui</a>
+                    </div>
+                </div>
             @endforelse
-            @endsection
-        </tbody>
-    </table>
-</div>
+        </div>
+    </div>
+@endsection
