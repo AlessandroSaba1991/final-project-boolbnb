bfe81edff3e4480580a58d8f00d46617136c520f
Rostrv <trovatorosario91@gmail.com>
2022-07-24 22:38:11 +0200

    fixed cards, add accordion menu on mobile

--- resources/js/Pages/AdvancedSearch.vue
+++ resources/js/Pages/AdvancedSearch.vue
@@ -1,31 +1,135 @@
 <template>
   <div class="page_body">
-    <div class="container">
+    <div class="container shadow-lg rounded mt-3 p-3">
       <header>
         <div class="page_title">
           <h1 class="text-center text-uppercase">
             Cerca la struttura perfetta per te
           </h1>
-          <div class="mb-5 destination">
+          <div class="mb-5 destination shadow">
             <label for="address" class="form-label">Destinazione</label>
             <input
               type="text"
-              class="form-control"
+              class="form-control mb-1"
               id="address"
               placeholder="inserisci una destinazione o una via"
               v-model="search"
               @keyup="callAddress()"
             />
             <div class="result" hidden></div>
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item border-0">
+                <h2 class="accordion-header" id="headingOne">
+                  <button
+                    class="accordion-button collapsed acc_btn"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Affina la tua ricerca
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body acc_body">
+                    <div class="mb-3">
+                      <label for="radius" class="form-label">
+                        Distanza max dal luogo
+                      </label>
+                      <input
+                        class="form-control fw-bold"
+                        id="radius"
+                        type="number"
+                        min="1"
+                        name="radius"
+                        value=""
+                        placeholder="KM"
+                        v-model="radius"
+                        oninput="this.value = Math.abs(this.value)"
+                        @keyup="getApartment()"
+                      />
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="rooms" class="form-label"
+                        >Numero di stanze</label
+                      >
+                      <input
+                        class="form-control fw-bold"
+                        id="rooms"
+                        type="number"
+                        min="1"
+                        max="100"
+                        name="rooms"
+                        value=""
+                        placeholder="Rooms"
+                        v-model="rooms"
+                        oninput="this.value = Math.abs(this.value)"
+                        @keyup="getApartment()"
+                      />
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="beds" class="form-label"
+                        >Numero di posti letto</label
+                      >
+                      <input
+                        class="form-control fw-bold"
+                        id="beds"
+                        min="1"
+                        max="100"
+                        type="number"
+                        name="beds"
+                        value=""
+                        placeholder="Beds"
+                        v-model="beds"
+                        oninput="this.value = Math.abs(this.value)"
+                        @keyup="getApartment()"
+                      />
+                    </div>
+
+                    <div class="multiple_service">
+                      <label for="services" class="form-label">Servizi</label>
+                      <select
+                        class="form-select"
+                        name="services"
+                        id="services"
+                        multiple
+                        aria-label="Default select example"
+                        v-model="serviceSelect"
+                      >
+                        <option disabled>seleziona dal menù</option>
+                        <option
+                          class="fw-bold"
+                          v-for="service in AllServices"
+                          :key="service.id"
+                          :value="service.id"
+                          @click="getApartment()"
+                        >
+                          {{ service.name }}
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
       <main>
         <div>
           <div class="row m-0 mb-5" style="width: 100%">
-            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 p-0 mb-5">
-              <div class="left_bar">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 p-0">
+              <div class="left_bar shadow">
                 <h2>Affina la tua ricerca</h2>
+
                 <div class="mb-3">
                   <label for="radius" class="form-label">
                     Distanza max dal luogo
@@ -124,12 +228,12 @@
               <div class="row show_results px-2">
                 <div
                   class="col-xs-12 col-sm-12 col-md-6 col-lg-4 g-3"
-                  style="height: auto"
+                  style="height: 420px"
                   v-for="apartment in apartments"
                   :key="apartment.id"
                 >
                   <div
-                    class="card rounded-3 shadow bg-body"
+                    class="card rounded-3 shadow bg-body border-warning"
                     style="height: 100%"
                   >
                     <img
@@ -137,14 +241,18 @@
                       alt=""
                       class="card-img-top img_resize"
                     />
-                    <div class="card-body">
-                      <h5 class="card-title text-center">
-                        {{ apartment.title }}
-                      </h5>
+                    <div
+                      class="
+                        card-body
+                        d-flex
+                        flex-column
+                        justify-content-between
+                      "
+                    >
+                      <h5 class="card-title">{{ apartment.title }}</h5>
                       <p class="card-text">
                         {{ apartment.description | truncate(99) }}...
                       </p>
-
                       <router-link
                         class="btn btn_orange text-uppercase text-white"
                         :to="{
@@ -152,7 +260,7 @@
                           params: { id: apartment.id },
                         }"
                       >
-                        check it
+                        Visualizza dettagli
                       </router-link>
                     </div>
                   </div>
@@ -298,7 +406,7 @@ export default {
   }
   .left_bar,
   .destination {
-    padding: 20px;
+    padding: 15px;
     color: white;
     text-transform: uppercase;
     font-weight: bold;
@@ -314,6 +422,28 @@ export default {
   .show_results {
     overflow-y: auto;
     height: 520px;
+  }
+
+  .acc_btn {
+    background-color: orange;
+    color: white;
+    text-transform: uppercase;
+  }
+
+  .acc_body {
+    background-color: orange;
+  }
+
+  @media (min-width: 746px) {
+    .accordion {
+      display: none;
+    }
+  }
+
+  @media (max-width: 745px) {
+    .left_bar {
+      display: none;
+    }
   }
 }
 </style>
